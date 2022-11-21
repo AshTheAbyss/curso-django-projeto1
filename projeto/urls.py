241d6c0c8751a21ffbecaaa3bc79bd3e70a00515
Alex Morais <ashtheabyss@live.com>
2022-11-21 13:48:11 -0300

    fim primeiro dia

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+from recipes.views import home, sobre, contato
 
+# return http responses
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home), #Home
+    path('sobre/', sobre), #sobre
+    path('contato/', contato), #contato
 ]
